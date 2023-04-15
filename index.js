19186b572134c28f1b6dffec1af3a30a97db8ce4
Timothy Ryon <tim@timryon.com>
2023-04-15 15:52:54 -0400

    finished assignment - ahhhh

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const cards = ["Guadalupe", "Ollie", "Aki"];
+const sentCards = [];
+
+
+function writeCards(cards) {
+ for (let thankYou = 0; thankYou < cards.length; thankYou++) {
+   sentCards.push(`Thank you, ${cards[thankYou]}, for the wonderful surprise gift!`);
+ }
+ return sentCards; }
+
+
+function countDown (integer){   
+    while (integer >= 0){
+        console.log(integer);
+        integer--;
+    }
+}
+
+countDown ();
